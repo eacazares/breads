6cb6ea7f2d0336bde077e55065a50e3624bcc799
eacazares <eacazar@gmail.com>
2023-04-23 12:07:22 -0700

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const breads = express.Router()
+const Bread = require('../models/bread.js')
 
 //index
 breads.get('/', (req, res) => {
-    res.send('this is the index at /breads')
+    res.send(Bread)
+})
+
+//show
+breads.get('/:arrayIndex', (req, res) => {
+    res.send(Bread[req.params.arrayIndex])
 })
 
 module.exports = breads
