b8c01aab3805d48b36ba71889b93f5dda3e2c26a
eacazares <eacazar@gmail.com>
2023-05-03 20:37:43 -0700

    create and new routes complete

--- views/index.jsx
+++ views/index.jsx
@@ -1,11 +1,17 @@
 const React = require ('react')
-const Default = require('./layouts/default')
+const Default = require('./layouts/Default')
 
 function Index ({breads}) {
     return (
         <Default>
             <h2>Index Page</h2>
             {/* <p>I have {breads[0].name} bread!</p> */}
+            <div className="newButton">
+                <a href="/breads/new"><button>Add a new bread</button></a>
+            </div>
+            <div className="backButton">
+                <a href="/breads"><button>Go back to the idnex</button></a>
+            </div>
             <ul>
                 {
                     breads.map((bread, index) => {
