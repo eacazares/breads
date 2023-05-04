bdcd94747dd92f8cdb2ead65fe7b961eb604e4ec
eacazares <eacazar@gmail.com>
2023-05-03 17:39:59 -0700

    sending index and show data

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
 //routes
 app.get('/', (req, res) => {
@@ -19,6 +20,10 @@ app.get('/', (req, res) => {
 //bread routes
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
+
+app.get('*', (req, res)=> {
+    res.send('404')
+})
 
 
 //listen
